0adbccf9ae03da2887295f1ee9cc19e662f37821
AlteriusOmega <TheRealSamChaney@gmail.com>
2018-11-10 20:11:08 -0600

    added new chords, scales, play notes button, fixed sound output length

--- bebop.js
+++ bebop.js
@@ -1,7 +1,29 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////// Get Input From HTML Page Forms ///////////////////////////////////
+////////////////////////// Create and configure synth object and sound output /////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+let synth = new Tone.Synth({
+  oscillator:{
+    type:'sine'
+  },
+  envelope : {
+    attack:0.01,
+    decay:0.7,
+    sustain:.1,
+    release:0.7
+  }
+
+});
+
+const gain = new Tone.Gain(0.9);
+gain.toMaster();
+synth.connect(gain);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Get Input From HTML Page Forms ///////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////
+$(".target" ).hide();
 
 $('button#runBebop').click(function(event){
 let chordFromHtml =  $('input#bebopIn').val() ;
@@ -10,12 +32,8 @@ let styleFromHtml = $('input[name=style]:checked').val();
 let directionFromHtml = $('input[name=direction]:checked').val();
 event.preventDefault();
 $('h1#output').html(BEBOPOBJ(chordFromHtml, keyFromHtml, styleFromHtml, directionFromHtml));
-soundNotes=null;
+// soundNotes=null;
 } );
-
- let testInput = 'G#Maj7';
-
-
 
 ////////////////////////////////////// Main Function ////////////////////////////////////////////// 
 function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
@@ -36,18 +54,24 @@ function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
   let styleIn = STYLE? STYLE.toLowerCase().replace(/ /g, '') : false;
   let directionIn = DIRECTION? DIRECTION.toLowerCase().replace(/ /g, '') : false;
   const allScales = {
-    'major': {'notes': [0,2,4,5,7,9,11]},
-    'minor': {'notes': [0,2,3,5,7,8,10]},
+    'major': {'notes': [0,2,4,5,7,9,11], 'mode':0, 'quality':'maj7'},
+    'dorian':{'notes':[0,2,3,5,7,9,10],'mode':2, 'quality':'min7'},
+    'phrygian':{'notes':[0,1,3,5,7,8,10],'mode':4, 'quality':'min7'},
+    'lydian':{'notes':[0,2,4,6,7,9,11],'mode':5, 'quality':'maj7'},
+    'mixolydian': {'notes': [0,2,4,5,7,9,10],'mode':7, 'quality':'dom7'},
+    'minor': {'notes': [0,2,3,5,7,8,10],'mode':9, 'quality':'min7'},
+    'locrian': {'notes': [0,1,3,5,6,8,10],'mode':11, 'quality':'min7b5'},
     'melodic minor': {'notes': [0,2,3,5,7,9,11]},
-    'mixolydian': {'notes': [0,2,4,5,7,9,10]},
-    'locrian': {'notes': [0,1,3,5,6,8,10]},
-    'whole tone': {'notes': [0,2,4,6,8,10]}
+    'whole tone': {'notes': [0,2,4,6,8,10]},
+    'h diminished': {'notes':[0,1,3,4,6,7,9,10]},
+    'w diminished' : {'notes':[0,2,3,5,6,8,9,11]}
   };
   const allChords = {
     'aug':{'names':['augmented','aug'], 'tones':[0,4,8], 'scale':'whole tone'},//scale in chords is default if no key is specified (chord assumed to be tonic)
     'min7b5':{'names':['min7b5','minor7b5','-7b5','half'], 'tones':[0,3,6,10], 'scale':'locrian'},
     'minmaj7':{'names': ['minmaj','-maj','minor-major','minormajor'], 'tones':[0,3,7,11], 'scale':'melodic minor'},
     'maj7':{'names':['maj'], 'tones': [0,4,7,11],'scale': 'major'},
+    'dim7':{'names':['dim','diminished','fully'],'tones':[0,3,6,9], 'scale':'w diminished'},
     'min7': {'names':['min','-','m'], 'tones':[0,3,7,10], 'scale':'minor'},
     'dom7':{'names':['dom','7'], 'tones':[0,4,7,10], 'scale':'mixolydian'},
     'maj':{'names':['', ' '], 'tones':[0,4,7], 'scale':'major'}//just a major triad in case there is no quality in input ex "Gb" will use Gb major triad MUST BE AT BOTTOM OF LIST
@@ -99,18 +123,18 @@ function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
   //chord object constructor
-  function chord (chordName){
+  function Chord (chordName, direction){
     let self = this;
     self.chordName = chordName;
-    self.strRoot = (function (chordIn){
+    self.strRoot = (function (chord){
     let root = 0;
-     if (chordIn[1]!='#' && chordIn[1]!='b')
+     if (chord[1]!='#' && chord[1]!='b')
        {  
-       root = chordIn[0].toUpperCase(); 
+       root = chord[0].toUpperCase(); 
        }
        else
        {
-       root = chordIn[0].toUpperCase()+chordIn[1];
+       root = chord[0].toUpperCase()+chord[1];
        }
        return (root);
        }(self.chordName) );//IIFE
@@ -124,11 +148,11 @@ function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
          return sharpsOrFlats;
         }(self.strRoot) );//IIFE
     self.intRoot = intNotes[self.sharpOrFlat][self.strRoot];
-    self.quality = (function (chordIn){
+    self.quality = (function (chord){
          let quality = 'quality not found!';
          for(let qualities in allChords){
            for(let names in allChords[qualities]['names']){
-             if((chordIn).match(allChords[qualities]['names'][names])){
+             if((chord).match(allChords[qualities]['names'][names])){
                 quality = allChords[qualities];
                 return quality;
                 break;
@@ -139,19 +163,76 @@ function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
     self.tones = (  function (rootIn, qualityIn){
          let tones = [];
          for(let tone in qualityIn['tones']){
+           if (direction=='down'){
+          tones.unshift( qualityIn['tones'][tone]+rootIn );
+           }
+           else{
            tones.push( qualityIn['tones'][tone]+rootIn );
+           }
          }
          return tones;
        }(self.intRoot, self.quality) );//IIFE
       }
   
-  //melody object constructor
-  function melody (chord, direction, rhythm, duration){
+  //Melody object constructor
+  function Melody (chord, key, rhythm, duration){
     var self = this;
     self.chord = chord;
-    self.chordScale = allScales[self.chord.quality['scale']]['notes'];
+    self.key = key;
+    self.strTonic = (function (key){
+      let root = 0;
+      if(self.key){
+       if (key[1]!='#' && key[1]!='b')
+          {  
+          root = key[0].toUpperCase(); 
+          }
+          else
+          {
+          root = key[0].toUpperCase()+key[1];
+         }
+        }
+        else{
+          root = 'No key';
+        }
+         return (root);
+         }(self.key) );//IIFE
+    self.minor = (function(key){
+      let isMinor = false;
+      if(self.key){
+        if( /min/.test(key) || /-/.test(key) ){
+          isMinor = true;
+        }
+      }
+       return isMinor;
+       }(self.key) );//IIFE
+    self.intTonic = (function(tonic){ 
+      let tonicOut=0;
+      if(self.minor){
+        tonicOut = bindNote( intNotes[self.chord.sharpOrFlat][self.strTonic] + 3 );
+      }
+      else{
+        tonicOut = intNotes[self.chord.sharpOrFlat][self.strTonic];
+      }
+      return tonicOut;
+    }(self.strTonic) );//IIFE   
+    self.chordScale = (function(){
+      let chordScaleOut = allScales[self.chord.quality['scale']]['notes'];
+        if(self.key ){
+          let mode = self.chord.intRoot - self.intTonic ;
+          if( allScales['major']['notes'].includes(mode) ) {
+            for(let scale in allScales){
+              if(allScales[scale].hasOwnProperty('mode')){
+                if(allScales[scale]['mode']==mode){
+                  chordScaleOut = allScales[scale]['notes'];
+                }
+              }
+            }
+          }
+       }
+        return chordScaleOut;
+      }());//IIFE
     self.tones = self.chord.tones;
-    self.direction = direction;
+    self.direction = directionIn;
     self.rhythm = rhythm;
     self.duration = duration;
     self.notes =[]; 
@@ -159,7 +240,7 @@ function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
         let scaleNotes = [];
         for (let note in self.chordScale){
           scaleNotes.push( self.chordScale[note]+self.chord.intRoot );
-        }
+          }
         return scaleNotes;
       }());//IIFE
     self.belowNote = function(tone){
@@ -239,9 +320,9 @@ function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
 ////////////////////////// Use Object Constructors to Generate Output /////// /////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-  let inputChord = new chord(chordIn);
+  let inputChord = new Chord(chordIn, directionIn);
   
-  let inputMelody = new melody(inputChord,'up','8th',1);
+  let inputMelody = new Melody(inputChord, keyIn, '8th',1);
 
   function getOutputMelody(){
     let notesOut = 0;
@@ -270,53 +351,41 @@ function BEBOPOBJ(CHORD, KEY, STYLE, DIRECTION) {
 // debugger;
   let outputNoteNames = getNoteNames(outputMelody, inputMelody.chord.sharpOrFlat);
 
+  let outNotesSpaces = (function (){ 
+    let outArr = [];
+    outputNoteNames.forEach(function(note, index){
+      outArr.push ( note = note+' ');
+  });
+  return outArr;
+}());//IIFE
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////// Create and configure synth object and sound output /////////////////////
+////////////////////////// Get notes for audio and play using Tone Transport //////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-
- let synth = new Tone.Synth({
-   oscillator:{
-     type:'triangle8'
-   },
-   envelope : {
-     attack:0.01,
-     decay:0.5,
-     sustain:0,
-     release:0.5
-   }
-
- });
-
-const gain = new Tone.Gain(0.9);
-gain.toMaster();
-synth.connect(gain);
 
 let soundNotes = (function(){
   let outNotes = [];
-  outNotes[0]='0';
+  outNotes[0]=1000000000;
   outputMelody.forEach(function(note, index){
     outNotes.push(getHz(note));
   })
-//   for (let note in inputMelody.noteNames ){
-// outNotes.push(inputMelody.noteNames[note] +'3');
-  // }
-  outNotes.push('0');
   return outNotes;
 }());//IIFE
 
 Tone.Transport.bpm.value = 90;
 Tone.Transport.swing = 0.5;
 
-let index = 0;
+let loopIndex = 0;
 
 let loop = new Tone.Loop(function(time){
-  let note = soundNotes[index % soundNotes.length];
+  let note = soundNotes[loopIndex % soundNotes.length];
   synth.triggerAttackRelease(note, "8n", time)
-  index++;
+  loopIndex++;
 }, "8n")
-loop.start(0).stop('2m');
 
-Tone.Transport.start();
+let melodyLength = soundNotes.length;
+loop.start(0).stop('0:'+melodyLength/2+':0');
+
+// Tone.Transport.start();
 ///////////////////////////////// Test Variables //////////////////////////////////
 // console.log ('reached the end of script');
 let testGetNoteNames = getNoteNames([-1,-5,-13], 'sharps');
@@ -325,11 +394,18 @@ let testGetHz = getHz(9);
 // debugger;
 
 //////////////////////////// Final Return Output //////////////////////////////////
-return (outputNoteNames);  
-
+$('.target').show();
+$('button#playNotes').click(function(event){
+  loopIndex =0;
+  event.preventDefault();
+  Tone.Transport.stop();
+  Tone.Transport.start();
+} );
+return (outNotesSpaces);  
 
 }
+///////////////////////////// Test Run Functions //////////////////////////////////
 function testRun(){
-  let testChord = 'CMaj7';
-  BEBOPOBJ(testChord);
+  let testInput = ['CMaj7','G Major','surround4','up'];
+  BEBOPOBJ(testInput);
 }
